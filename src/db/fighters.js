2250b2b3fecfdf8ef6076fa25e5e9a4f7c8f9079
Daniel Lousa <dlousa92@gmail.com>
2020-03-09 00:14:53 -0400

    create initial fighters

---
+++ src/db/fighters.js
@@ -0,0 +1,87 @@
+const cyberSamurai = {
+  name: "Cyber Samurai",
+  moves: [
+    {
+      name: "Hidden Blade",
+      damage: 10
+    },
+    {
+      name: "Cyber Powered Uppercut",
+      damage: 20
+    },
+    {
+      name: "Cyber Sword Slash",
+      damage: 30
+    },
+    {
+      name: "Ultra Sword Flurry",
+      damage: 40
+    }
+  ]
+};
+
+const alienGunslinger = {
+  name: "Alien Gunslinger",
+  moves: [
+    {
+      name: "Quick Draw",
+      damage: 10
+    },
+    {
+      name: "Hidden Gun",
+      damage: 20
+    },
+    {
+      name: "Triple Shot",
+      damage: 30
+    },
+    {
+      name: "Super Powered Shotgun",
+      damage: 40
+    }
+  ]
+};
+
+const underlingMech = {
+  name: "Underling Mech",
+  moves: [
+    {
+      name: "Drill Lunge",
+      damage: 10
+    },
+    {
+      name: "Charge",
+      damage: 20
+    },
+    {
+      name: "Burrow Attack",
+      damage: 30
+    },
+    {
+      name: "Shoulder Demolition Rockets",
+      damage: 40
+    }
+  ]
+};
+
+const antiMatterRobot = {
+  name: "Antimatter Robot",
+  moves: [
+    {
+      name: "Matter Beam",
+      damage: 10
+    },
+    {
+      name: "Interdimensional Punch",
+      damage: 20
+    },
+    {
+      name: "Gravity Gun",
+      damage: 30
+    },
+    {
+      name: "Anti Matter Cannon Arms",
+      damage: 40
+    }
+  ]
+};
